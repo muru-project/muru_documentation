1de1fce17302778ff103f70ddc188c64750beb96
octavioaguila <oeaguila@uc.cl>
2023-08-08 10:14:55 -0400

    titulo

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "Basic Sphinx Example Project"
-copyright = "2022, Read the Docs core team"
-author = "Read the Docs core team"
+project = "MURU Documentation"
+copyright = "2023, MURU team"
+author = "Muru team"
 
 
 # -- General configuration ---------------------------------------------------
